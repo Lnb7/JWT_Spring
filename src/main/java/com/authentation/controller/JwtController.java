8fe9f48e486d141abf03cb8b62aa791d89fbced0
loknathbanerjee <lokabanerjee@gmail.com>
2023-07-27 22:53:01 +0530

    jwt token creation

---
+++ src/main/java/com/authentation/controller/JwtController.java
@@ -0,0 +1,51 @@
+package com.authentation.controller;
+
+import com.authentation.helper.JwtUtil;
+import com.authentation.model.JwtRequest;
+import com.authentation.model.JwtResponse;
+import com.authentation.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JwtController {
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/token")
+    public ResponseEntity<?> generateToken(@RequestBody JwtRequest jwtRequest){
+        System.out.println(jwtRequest);
+
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(),jwtRequest.getPassword()));
+        } catch (UsernameNotFoundException e){
+            e.printStackTrace();
+            throw new UsernameNotFoundException("UsernameNotFoundException");
+        } catch (BadCredentialsException e){
+            e.printStackTrace();
+            throw new BadCredentialsException("BadCredentialsException");
+        }
+
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(jwtRequest.getUsername());
+        String token = jwtUtil.generateToken(userDetails);
+
+        System.out.println("token : " + token);
+
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+}
---
+++ src/main/java/com/authentation/model/JwtRequest.java
@@ -0,0 +1,15 @@
+package com.authentation.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtRequest {
+    String username;
+    String password;
+}
